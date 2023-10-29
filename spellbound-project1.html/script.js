1ea5003269912d1dc17b0b2881d09a7fb1977715
erincpaterson <146275636+erincpaterson@users.noreply.github.com>
2023-10-29 16:29:12 -0700

    update

--- spellbound-project1.html/script.js
+++ spellbound-project1.html/script.js
@@ -1,25 +1,30 @@
-document.querySelectorALL(".carousel").forEach(carousel => {
-const items = carousel.querySelectorALL(".carouselitem");
-const buttonsHtml = Array.from(items, () => {
-    return '<span class="carouselbutton"><span>';
-    })
-});
-
-carousel.insertAdjacentHTML("beforeend", '
-    <div class="carouselnav">
-        ${buttonsHtml.join("")}
-    </div>
-');
-
-    const buttons = carousel.querySelectorAll(".carouselbutton");
-
-    buttons.forEach(button, i) => {
-        button.addEventListener("click", () => {
-            items.forEach(item => item.classList.remove("carouselitem-selected"));
-            buttons.forEach(button => button.classList.remove(carouselbutton-selected));
-
-            items[i].classList.add("carouselitem-selected");
-            button.classList.add("carouselbutton-selcted");
-        });
-    });
-});
+$('.hero').slick({
+    centerMode: true,
+    arrows: true,
+    dots: true,
+    centerPadding: '21%',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+          
+  // https://kenwheeler.github.io/slick/
